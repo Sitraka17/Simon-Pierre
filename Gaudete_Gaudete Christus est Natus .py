3cbe91bc1111c6929881e60a7ac787584178edc7
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-07 19:51:02 +0100

    Update Gaudete_Gaudete Christus est Natus .py

--- Gaudete_Gaudete Christus est Natus .py
+++ Gaudete_Gaudete Christus est Natus .py
@@ -1,1 +1,32 @@
 ###  Gaudete, Gaudete Christus est Natus !!!!!
+
+Gaudete, gaudete!
+Christus est natus
+Ex Maria Virgine,
+Gaudete!
+
+Tempus adest gratiae
+Hoc quod optabamus,
+Carmina laetitiae
+Devote redamus.
+
+Ergo nostra concio
+Psallat iam in lustro,
+Benedicat Domino:
+Salus Regi nostro.
+
+Deo nostro gratias
+Angilium cantico,
+Hodie apparuit:
+Quem profetae dixerunt.
+
+In terra canunt angeli,
+Laetantur archangeli;
+Transeunt omnes meli,
+Christum Regem adorent.
+
+Gaudete, gaudete!
+Christus est natus
+Ex Maria Virgine,
+Gaudete!
+
