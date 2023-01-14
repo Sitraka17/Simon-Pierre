29a75d45e38ada62d7d98e79af7040580ca082c4
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-14 17:59:51 +0100

    Update Gaudete_Gaudete Christus est Natus .py

--- Gaudete_Gaudete Christus est Natus .py
+++ Gaudete_Gaudete Christus est Natus .py
@@ -4,7 +4,7 @@ def display_chanson_lyrics(chanson):
   if chanson == "Gaute":
     print("Gaudete, gaudete! Christus est natus Ex Maria Virgine, Gaudete!  Tempus adest gratiae Hoc quod optabamus,Carmina laetitiae Devote redamus.")
   elif chanson == "Rejoyce":
-    print("Lyrics for Gaute in English")
+    print("Rejoice, rejoice![a] Christ is born Of the Virgin Mary – Rejoice!")
   else:
     print("Invalid chanson selection.")
 
